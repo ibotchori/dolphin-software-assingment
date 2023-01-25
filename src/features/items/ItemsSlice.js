64e5ddd6562028c7c4a25c775744ae48c181554c
Irakli <i.botchori@gmail.com>
2023-01-25 11:48:21 +0400

    style: change items page style

--- src/features/items/ItemsSlice.js
+++ src/features/items/ItemsSlice.js
@@ -1,6 +1,17 @@
 import { createSlice } from '@reduxjs/toolkit'
 
-const initialState = []
+const initialState = [
+  { id: 41, title: 'Gawen', price: '0', quantity: '718' },
+  { id: 42, title: 'Mollie', price: '1975', quantity: '4990' },
+  { id: 43, title: 'Alvis', price: '1048', quantity: '74' },
+  { id: 44, title: 'Abigael', price: '1148', quantity: '32' },
+  { id: 45, title: 'Mavra', price: '670', quantity: '450' },
+  { id: 46, title: 'Jacklin', price: '20', quantity: '633' },
+  { id: 47, title: 'Zed', price: '476', quantity: '902' },
+  { id: 48, title: 'Clair', price: '197', quantity: '570' },
+  { id: 49, title: 'Stewart', price: '3590', quantity: '3100' },
+  { id: 50, title: 'Tore', price: '53', quantity: '156' },
+]
 
 const itemsSlice = createSlice({
   name: 'items',
--- src/pages/UserItem/UserItem.jsx
+++ src/pages/UserItem/UserItem.jsx
@@ -53,49 +53,27 @@ const UserItem = () => {
     reset()
   }
   return (
-    <div className='bg-gray-100 w-full h-screen sm:py-10 flex flex-col justify-between'>
-      <PageTitle path='/' />
-      <div className='flex justify-center h-full '>
-        <div className='sm:w-[60%] w-full bg-white rounded-xl p-4 sm:p-10 '>
-          <p className=' text-xl font-bold pb-10'>გამარჯობა {name}</p>
-          <form onSubmit={handleSubmit(onSubmit)} encType='multipart/form-data'>
-            <div className='flex flex-col lg:flex-row sm:justify-between gap-3 items-center pb-5 '>
-              <div className='w-[21.875rem] sm:w-[25rem] '>
-                <Input
-                  label='დასახელება'
-                  name='title'
-                  register={register}
-                  errorMessage={errors.title?.message}
-                  dirtyFields={dirtyFields.title}
-                />
-              </div>
-              <div className='w-[21.875rem] sm:w-[25rem]'>
-                <Input
-                  label='ფასი'
-                  type={'number'}
-                  name='price'
-                  register={register}
-                  errorMessage={errors.price?.message}
-                  dirtyFields={dirtyFields.price}
-                />
-              </div>
-              <div className='w-[21.875rem] sm:w-[25rem]'>
-                <Input
-                  label='რაოდენობა'
-                  type={'number'}
-                  name='quantity'
-                  register={register}
-                  errorMessage={errors.quantity?.message}
-                  dirtyFields={dirtyFields.quantity}
-                />
-              </div>
-            </div>
-            <div className='w-full flex pr-3 lg:pr-0  justify-end'>
-              <SubmitButton text='დამატება' px='px-10' />
-            </div>
-          </form>
-          {items.length > 0 && <Table data={items} />}
+    <div className='bg-gray-100 w-full h-screen  sm:p-20 flex flex-col justify-between'>
+      <div className='px-4 sm:px-6 lg:px-8'>
+        <div className='sm:flex sm:items-center'>
+          <div className='sm:flex-auto'>
+            <h1 className='text-xl font-semibold text-gray-900'>
+              გამარჯობა {name}
+            </h1>
+            <p className='mt-2 text-sm text-gray-700'>
+              აქ შეგიძლიათ დაამატოთ მომხმარებელის ნივთები
+            </p>
+          </div>
+          <div className='mt-4 sm:mt-0 sm:ml-16 sm:flex-none'>
+            <button
+              type='button'
+              className='inline-flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:w-auto'
+            >
+              ნივთის დამატება
+            </button>
+          </div>
         </div>
+        <Table data={items} />
       </div>
     </div>
   )
