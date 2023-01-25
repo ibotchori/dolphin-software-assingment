876273f6b8764065a33bfb0d0e6cd03cadda5ecd
Irakli <i.botchori@gmail.com>
2023-01-25 11:47:44 +0400

    style: change submit button color

--- src/components/Buttons/SubmitButton.jsx
+++ src/components/Buttons/SubmitButton.jsx
@@ -4,7 +4,7 @@ const Button = ({ text, px }) => {
   return (
     <button
       type='submit'
-      className={`${px} py-3  bg-mainButtonColor hover:bg-hoverButtonColor  focus:bg-focusButtonColor  rounded-lg  text-center  text-lg  tracking-wide text-white  font-semibold`}
+      className={`${px}  inline-flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:w-auto`}
     >
       {text}
     </button>
