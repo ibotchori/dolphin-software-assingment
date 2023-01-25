60456400074d3bd9772dd256f60d8ff375429ab7
Irakli <i.botchori@gmail.com>
2023-01-25 18:55:46 +0400

    chore: navigate home page from item list if user is not added

--- src/hooks/useItemList.js
+++ src/hooks/useItemList.js
@@ -1,7 +1,16 @@
+import { useEffect } from 'react'
 import { useSelector } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
 const useItemList = () => {
   const { name } = useSelector((state) => state.user)
   const items = useSelector((state) => state.items)
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    if (!name) {
+      navigate('/')
+    }
+  }, [name, navigate])
 
   return {
     name,
